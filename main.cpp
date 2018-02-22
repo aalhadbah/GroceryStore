ef92afc2a87c3689c80680deff2ae0b75fd5f4f9
aalhadbah <aalhadbah@mail.csuchico.edu>
2018-02-21 17:29:17 -0800

    First step

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
+//author: Abdulaziz Alhadbah
 #include<iostream>
 #include<string>
+
 using namespace std;
 
 int main()
 {
   string list[5]; //array of 5 strings
-  int numItems = 0;
+  int numItems = 5;
   char input;
-
+do{
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+}
+
+while(( input!='q' ) && ( input!='Q' ));
 
   return 0;
 }
